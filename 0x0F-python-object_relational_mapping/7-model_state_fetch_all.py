7acece980a68509bf568bb25f145f51a2028bf4c
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2024-01-13 21:29:23 +0100

    Add files via upload

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,39 +1,17 @@
 #!/usr/bin/python3
+"""Start link class to table in database
 """
-Script that lists all State objects from the database hbtn_0e_6_usa
-"""
-
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    # Get command line arguments: mysql username, mysql password, and database name
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Create connection string
-    connection_string = 'mysql+mysqldb://{}:{}@localhost:3306/{}'\
-        .format(mysql_username, mysql_password, db_name)
-
-    # Create engine and bind it to the Base's metadata
-    engine = create_engine(connection_string)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
-    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
     session = Session()
-
-    # Query all State objects and order by id
-    states = session.query(State).order_by(State.id).all()
-
-    # Print the results
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-
-    # Close the session
-    session.close()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
