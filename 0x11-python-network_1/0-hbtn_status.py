ff75767bcb62f990de0942686e1992ebad678a38
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2024-01-26 19:48:33 +0100

    Create 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script fetches the content of a URL using urllib package and displays the body of the response.
+"""
+
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
